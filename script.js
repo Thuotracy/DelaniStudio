78be2d1be5608dcdcf5e194c994c6c7f6b18524e
tracy <tracyjacobs775@gmail.com>
2021-11-24 15:47:41 +0300

    save the html images

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $("#img img").click(function(){
+     $(".show").toggle();
+     $(".hidden").toggle();   
+    });
+
+
+    $(".img p").click(function(){  
+     $("#hidden").toggle();
+     $("#show").toggle();
+        
+    });
+});
+$("#work1").hover(function(){
+    $(".blackstudio").toggle();
+   });
+   $("#work2").hover(function(){
+    $(".brown").toggle();
+   });
+   $("#work3").hover(function(){
+    $(".ontario").toggle();
+   });
+   $("#work4").hover(function(){
+    $(".Black").toggle();
+   });
+   $("#work5").hover(function(){
+    $(".elena").toggle();
+   });
+   $("#work6").hover(function(){
+    $(".button").toggle();
+   });
+   $("#work7").hover(function(){
+    $(".burned").toggle();
+   });
+   $("#work8").hover(function(){
+    $(".giraffe").toggle();
+   });
